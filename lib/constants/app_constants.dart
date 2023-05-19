4992c9e8fa4fe6ce2b4998b2b5a12538dfd6c6de
nain93 <rnrb555@gmail.com>
2023-05-20 01:47:59 +0900

    chore: constants 파일 수정

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,4 +1,11 @@
 class AppConstants {
-  static const String replaceableText = "file://";
+  static const String replaceableText = 'file://';
   static const List<String> imageExtensions = ['jpg', 'png', 'jpeg', 'gif'];
+
+  static const appName = 'moa_app';
+
+  static const mobileMaxWith = 850;
+  static const tableMaxWith = 1100;
+
+  static const imageLogo = 'res/images/logo.png';
 }
