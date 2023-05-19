250397537202e48595446092e840079bc26bf2f5
nain93 <rnrb555@gmail.com>
2023-05-20 01:47:59 +0900

    chore: constants 파일 수정

--- lib/utils/helpers.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:moa_app/utils/constants.dart';
-// import 'package:moa_app/utils/localization.dart';
-
-// void alert(String message) => Get.snackbar(appName, message);
-
-// void alertWarning(String message) => Get.snackbar(t("WARNING"), message);
-
-// void alertError(String message) => Get.snackbar(t("ERROR"), message);
