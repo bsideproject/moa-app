a683e622cfb0767224f804cd623f42218380d847
paulracooni <paulracooni@gmail.com>
2023-05-27 01:20:02 +0900

    feat: Impl TokenRepository with FlutterSecureStorage

--- lib/repositories/token_repository.dart
+++ lib/repositories/token_repository.dart
@@ -1,66 +1,32 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:moa_app/utils/logger.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
-abstract class ITokenRepository {
-  Future<Database> initDB();
-  Future<void> setToken({required String token});
-  Future<void> removeToken();
-  Future<Object?> getToken();
-}
 
-class TokenRepository implements ITokenRepository {
+class TokenRepository {
   const TokenRepository._();
   static TokenRepository instance = const TokenRepository._();
 
-  @override
-  Future<Database> initDB() async {
-    var database = openDatabase(
-      // 데이터베이스 경로를 지정합니다. 참고: `path` 패키지의 `join` 함수를 사용하는 것이
-      // 각 플랫폼 별로 경로가 제대로 생성됐는지 보장할 수 있는 가장 좋은 방법입니다.
-      join(await getDatabasesPath(), 'token_database.db'),
+  static const storage = FlutterSecureStorage();
 
-      onCreate: (db, version) {
-        // 데이터베이스에 CREATE TABLE 수행
-        return db.execute(
-          'CREATE TABLE token(token TEXT)',
-        );
-      },
-      version: 1,
-    );
+  static const String key = 'SecuredAuthToken';
 
-    var db = await database;
-    return db;
-  }
-
-  @override
   Future<void> setToken({required String token}) async {
-    var db = await initDB();
     try {
-      await db.insert('token', {'token': token},
-          conflictAlgorithm: ConflictAlgorithm.replace);
+      await storage.write(key:key, value:token);
     } catch (e) {
       logger.d(e);
     }
   }
 
-  @override
   Future<void> removeToken() async {
-    var db = await initDB();
     try {
-      await db.delete('token');
+      await storage.delete(key: key);
     } catch (e) {
       logger.d(e);
     }
   }
 
-  @override
   Future<Object?> getToken() async {
-    var db = await initDB();
-    var res = await db.query('token');
-    if (res.isNotEmpty) {
-      return res.first['token'];
-    }
-    return null;
+    return await storage.read(key: key);
   }
 }
