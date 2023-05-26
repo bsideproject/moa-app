a0c6feea0c00f557b672fe65edd67cc58e34b797
nain93 <rnrb555@gmail.com>
2023-05-27 01:40:20 +0900

    chore: add abstract class token_repository

--- lib/repositories/token_repository.dart
+++ lib/repositories/token_repository.dart
@@ -1,23 +1,29 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:moa_app/utils/logger.dart';
 
+abstract class ITokenRepository {
+  Future<void> setToken({required String token});
+  Future<void> removeToken();
+  Future<Object?> getToken();
+}
 
-class TokenRepository {
+class TokenRepository implements ITokenRepository {
   const TokenRepository._();
   static TokenRepository instance = const TokenRepository._();
 
   static const storage = FlutterSecureStorage();
-
   static const String key = 'SecuredAuthToken';
 
+  @override
   Future<void> setToken({required String token}) async {
     try {
-      await storage.write(key:key, value:token);
+      await storage.write(key: key, value: token);
     } catch (e) {
       logger.d(e);
     }
   }
 
+  @override
   Future<void> removeToken() async {
     try {
       await storage.delete(key: key);
@@ -26,7 +32,14 @@ class TokenRepository {
     }
   }
 
+  @override
   Future<Object?> getToken() async {
-    return await storage.read(key: key);
+    try {
+      var token = await storage.read(key: key);
+      return token;
+    } catch (e) {
+      logger.d(e);
+    }
+    return null;
   }
 }
