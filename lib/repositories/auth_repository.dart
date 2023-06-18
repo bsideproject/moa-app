0477ed087f3d5f974e7aa0ece3cc6eb2f92d8eec
paulracooni <paulracooni@gmail.com>
2023-06-19 00:41:46 +0900

    refact: Cleaning auth_repository.dart

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -12,12 +12,35 @@ import 'package:moa_app/utils/logger.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:uuid/uuid.dart';
 
+
+class AuthDto {
+  // 임시로Moa back-end 로 보내기 위한 데이터를 관리하는 객체
+  // 성공, 실패에 대한 응답을 담고 있다.
+  AuthDto.fail(): isSuccess=false;
+
+  AuthDto.success({
+    required String this.id,
+    required String this.email,
+    required String this.token,
+    required String this.platform,
+  }): isSuccess=true;
+
+  String? id;
+  String? email;
+  String? token;
+  String? platform;
+  final bool isSuccess;
+
+}
+
+
 abstract class IAuthRepository {
   Future<void> googleLogin();
   Future<void> kakaoLogin();
   Future<void> naverLogin();
   Future<void> appleLogin();
 }
+
 
 class AuthRepository implements IAuthRepository {
   const AuthRepository._();
@@ -26,229 +49,260 @@ class AuthRepository implements IAuthRepository {
   @override
   Future<void> googleLogin() async {
     late GoogleSignInAccount? user;
-    var googleSignIn = GoogleSignIn();
+    var googleSignIn = GoogleSignIn(
+        signInOption: SignInOption.standard,
+        clientId: Config().googleClientId,
+
+    );
 
     if (kIsWeb) {
+      // TODO: signIn 이 WEB에서 처음에 안되서 다시 로그인 시도 해야 로그인 되는 현상이 있음
+      // 이거 문제가 뭔지 모르겠음...
       user = await googleSignIn.signInSilently();
       user ??= await googleSignIn.signIn();
     } else {
       user = await googleSignIn.signIn();
     }
 
-    if (user != null) {
-      var token = await user.authentication;
-      var res = await dio.post(
-        '/api/v1/user/oauth',
-        data: {
-          'platform': 'google',
-          'id': user.id,
-          'email': user.email,
-        },
-        options: Options(
-          headers: {'oauth-token': token.idToken},
-        ),
-      );
+    if (user == null) { return ; }
 
-      if (res.data['access_token'].isNotEmpty) {
-        await TokenRepository.instance
-            .setToken(token: res.data['access_token']);
-      }
-    }
+    var token = await user.authentication;
+
+    AuthDto authDto = token.idToken != null ?
+      AuthDto.success(
+        platform: 'google',
+        id: user.id,
+        email: user.email,
+        token: token.idToken!,):
+      AuthDto.fail();
+
+      await _loginAndSetTokenWith(authDto);
   }
 
   @override
   Future<void> kakaoLogin() async {
 
-    late String? token;
-    if (kIsWeb) {
+    AuthDto authDto = kIsWeb ?
+      await _kakaoLoginWeb():
+      await _kakaoLoginMobile();
 
-      // Kakao web authentication
-      var respAuth = await FlutterWebAuth.authenticate(
-        url: Uri.https('kauth.kakao.com','oauth/authorize', {
-          'client_id': Config().kakaoClientId,
-          'redirect_uri': Config().socialRedirectUrl,
-          'response_type': 'code',
-          'state': const Uuid().v4()
-        }).toString(),
-        callbackUrlScheme: 'moaapp',
-      );
+    if (authDto.isSuccess){
+      await _loginAndSetTokenWith(authDto);
+    }
+  }
 
-      // Handling Authentication Exception
-      String? authCode = Uri.parse(respAuth).queryParameters['code'];
-      if (authCode == null){
-        String resp = Uri.parse(respAuth).toString();
-        throw 'Fail authentication with kakao\n- $resp';
-      }
+  Future<AuthDto> _kakaoLoginWeb() async {
 
-      // Request Token
-      var respToken = await Dio().post(
-        'https://kauth.kakao.com/oauth/token',
-        data: {
-          'grant_type': 'authorization_code',
-          'client_id': Config().kakaoClientId,
-          'redirect_uri': Config().socialRedirectUrl,
-          'code': authCode,
+    // Kakao web authentication
+    var respAuth = await FlutterWebAuth.authenticate(
+      url: Uri.https('kauth.kakao.com','oauth/authorize', {
+        'client_id': Config().kakaoClientId,
+        'redirect_uri': Config().socialRedirectUrl,
+        'response_type': 'code',
+        'state': const Uuid().v4()
+      }).toString(),
+      callbackUrlScheme: 'moaapp',
+    );
+
+    // Handling Authentication Exception
+    String? authCode = Uri.parse(respAuth).queryParameters['code'];
+    if (authCode == null){
+      String resp = Uri.parse(respAuth).toString();
+      throw 'Fail authentication with kakao\n- $resp';
+    }
+
+    // Request Token
+    var respToken = await Dio().post(
+      'https://kauth.kakao.com/oauth/token',
+      data: {
+        'grant_type': 'authorization_code',
+        'client_id': Config().kakaoClientId,
+        'redirect_uri': Config().socialRedirectUrl,
+        'code': authCode,
+      },
+      options: Options(
+        headers: {
+          HttpHeaders.contentTypeHeader:
+          'application/x-www-form-urlencoded;charset=utf-8'
         },
-        options: Options(
-          headers: {
-            HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded;charset=utf-8'
-          },
-        ),
-      );
+      ),
+    );
 
-      token = respToken.data['access_token'];
-
-    } else {
-      var isInstalled = await isKakaoTalkInstalled();
-      OAuthToken oauthToken = isInstalled
-          ? await UserApi.instance.loginWithKakaoTalk()
-          : await UserApi.instance.loginWithKakaoAccount();
-      token = oauthToken.accessToken;
+    if (respToken.statusCode != 200) {
+      return AuthDto.fail();
     }
 
-    if (token != null){
-      var response = await Dio().get(
-        'https://kapi.kakao.com/v2/user/me',
-        options: Options(
-          headers: {
-            HttpHeaders.authorizationHeader: 'Bearer $token'
-          },
-        ),
+    return await _kakaoRequestUserInfo(
+        token: respToken.data['access_token']
+    );
+  }
+
+  Future<AuthDto> _kakaoLoginMobile() async {
+
+    var isInstalled = await isKakaoTalkInstalled();
+
+    OAuthToken oauthToken = isInstalled
+        ? await UserApi.instance.loginWithKakaoTalk()
+        : await UserApi.instance.loginWithKakaoAccount();
+
+    return await _kakaoRequestUserInfo(
+        token: oauthToken.accessToken
+    );
+  }
+
+  Future<AuthDto> _kakaoRequestUserInfo({
+    required String token }) async {
+
+    var response = await Dio().get(
+      'https://kapi.kakao.com/v2/user/me',
+      options: Options(
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token'
+        },
+      ),
+    );
+
+    if (response.data.isNotEmpty) {
+      return AuthDto.success(
+          id: response.data['id'].toString(),
+          email: response.data['kakao_account']['email'],
+          token: token,
+          platform: 'kakao',
       );
-
-      if (response.data.isNotEmpty) {
-        var res = await dio.post(
-          '/api/v1/user/oauth',
-          data: {
-            'platform': 'kakao',
-            'id': response.data['id'],
-            'email': response.data['kakao_account']['email'],
-          },
-          options: Options(
-            headers: {
-              'oauth-token': token,
-            },
-          ),
-        );
-
-        if (res.data['access_token'].isNotEmpty) {
-          await TokenRepository.instance
-              .setToken(token: res.data['access_token']);
-        }
-      }
     }
+    return AuthDto.fail();
   }
 
   @override
   Future<void> naverLogin() async {
+    AuthDto authDto = kIsWeb ?
+      await _naverLoginWeb():
+      await _naverLoginMobile();
+
+    await _loginAndSetTokenWith(authDto);
+  }
+
+  Future<AuthDto> _naverLoginWeb() async {
+    // Present the dialog to the user
+    var resultAuth = await FlutterWebAuth.authenticate(
+      url: Uri.https('nid.naver.com', 'oauth2.0/authorize', {
+        'client_id': Config().naverClientId,
+        'redirect_uri': Config().socialRedirectUrl,
+        'response_type': 'code',
+        'state': const Uuid().v4()
+      }).toString(),
+      callbackUrlScheme: 'moaapp',
+    );
+    // Extract code from resulting url
+    // result = http://localhost:8080/sign-in?code=dXi4LtnBGvkWwupyr7&state=test
+    var code = Uri
+        .parse(resultAuth)
+        .queryParameters['code'];
+
+    var resToken = await Dio().get(
+      '${Config().moaDjangoUrl}/proxy_auth/naver_token',
+      queryParameters: {
+        'client_id': Config().naverClientId,
+        'client_secret': Config().naverClientSecret,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'state': const Uuid().v4(),
+      },
+    );
+
+    if (resToken.statusCode != 200) {
+      return AuthDto.fail();
+    }
+
+    return AuthDto.success(
+        id: resToken.data['id'],
+        email: resToken.data['email'],
+        token: resToken.data['access_token'],
+        platform: 'naver',
+    );
+  }
+
+  Future<AuthDto> _naverLoginMobile() async {
     late NaverAccessToken token;
     late NaverLoginResult user;
-    if (kIsWeb) {
-      // Web NAVER social login
 
-      // Present the dialog to the user
-      var resultAuth = await FlutterWebAuth.authenticate(
-        url: Uri.https('nid.naver.com','oauth2.0/authorize', {
-          'client_id': Config().naverClientId,
-          'redirect_uri': Config().socialRedirectUrl,
-          'response_type': 'code',
-          'state': const Uuid().v4()
-        }).toString(),
-        callbackUrlScheme: 'moaapp',
-      );
-      // Extract code from resulting url
-      // result = http://localhost:8080/sign-in?code=dXi4LtnBGvkWwupyr7&state=test
-      var code = Uri.parse(resultAuth).queryParameters['code'];
+    // Non web Naver login option
+    user = await FlutterNaverLogin.logIn();
+    token = await FlutterNaverLogin.currentAccessToken;
 
-      var resToken = await Dio().get(
-          '${Config().moaDjangoUrl}/proxy_auth/naver_token',
-          queryParameters: {
-            'client_id': Config().naverClientId,
-            'client_secret': Config().naverClientSecret,
-            'grant_type': 'authorization_code',
-            'code': code,
-            'state': const Uuid().v4(),
-          },
-      );
-
-      if (resToken.statusCode==200) {
-        var res = await dio.post(
-          '/api/v1/user/oauth',
-          data: {
-            'platform': 'naver',
-            'id': resToken.data['id'],
-            'email': resToken.data['email'],
-          },
-          options: Options(
-            headers: {'oauth-token': resToken.data['access_token']},
-          ),
-        );
-
-        if (res.data['access_token'].isNotEmpty) {
-          await TokenRepository.instance
-              .setToken(token: res.data['access_token']);
-        }
-      }
-
-    } else {
-      // Non web Naver login option
-      user = await FlutterNaverLogin.logIn();
-      token = await FlutterNaverLogin.currentAccessToken;
-      if (user.status != NaverLoginStatus.loggedIn) {
-        return;
-      }
-
-      if (token.accessToken.isNotEmpty) {
-        var res = await dio.post(
-          '/api/v1/user/oauth',
-          data: {
-            'platform': 'naver',
-            'id': user.account.id,
-            'email': user.account.email,
-          },
-          options: Options(
-            headers: {'oauth-token': token.accessToken},
-          ),
-        );
-
-        if (res.data['access_token'].isNotEmpty) {
-          await TokenRepository.instance
-              .setToken(token: res.data['access_token']);
-        }
-      }
+    if (user.status != NaverLoginStatus.loggedIn) {
+      return AuthDto.fail();
     }
+
+    return AuthDto.success(
+        id: user.account.id,
+        email: user.account.email,
+        token: token.accessToken,
+        platform: 'naver',
+    );
   }
 
   @override
   Future<void> appleLogin() async {
     var credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-      webAuthenticationOptions: kIsWeb? WebAuthenticationOptions(
-        clientId: 'asd',
-        redirectUri: Uri.parse(Config().socialRedirectUrl)
-      ) : null
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        // TODO: Apple login은 clientId가 없어 아직 테스트 못해봄
+        webAuthenticationOptions: kIsWeb ? WebAuthenticationOptions(
+            clientId: Config().appleClientId,
+            redirectUri: Uri.parse(Config().socialRedirectUrl)
+        ) : null
     );
 
     if (credential.identityToken != null && credential.userIdentifier != null) {
-      var res = await dio.post(
-        '/api/v1/user/oauth',
-        data: {
-          'platform': 'apple',
-          'id': credential.userIdentifier,
-          'email': credential.email,
-        },
-        options: Options(
-          headers: {'oauth-token': credential.identityToken},
-        ),
-      );
-
-      if (res.data['access_token'].isNotEmpty) {
-        await TokenRepository.instance
-            .setToken(token: res.data['access_token']);
-      }
+      await _loginAndSetTokenWith(AuthDto.success(
+        id: credential.userIdentifier!,
+        email: credential.email!,
+        token: credential.identityToken!,
+        platform: 'apple',
+      ));
     }
   }
+
+  Future<void> _loginAndSetTokenWith(AuthDto authDto) async {
+    if(!authDto.isSuccess) {
+      // TODO: 로그인에 실패 예외처리 하기, 메세지를 띄운다든지 등등
+      logger.d('Login Fail');
+      return ;
+    }
+
+    var res = await dio.post(
+      '/api/v1/user/oauth',
+      data: {
+        'platform': authDto.platform,
+        'id': authDto.id,
+        'email': authDto.email,
+      },
+      options: Options(
+        headers: {'oauth-token': authDto.token},
+      ),
+    );
+
+    if (res.statusCode != 200) {
+      // TODO: 우리 백엔드 쪽, 처리 실패 관련 예외처리 하기
+      logger.d('Moa-Spring Error - $res.statusCode\n$res.data');
+      return ;
+    }
+
+    await TokenRepository.instance
+        .setToken(token: res.data['access_token']);
+
+    // 로그인 성공
+    logger.d(
+        'login success\n'
+        'platform: ${authDto.platform}\n'
+        'id      : ${authDto.id}\n'
+        'email   : ${authDto.email}\n'
+        'token   : ${authDto.token}\n'
+    );
+  }
+
+
 }
--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -9,16 +9,19 @@ class Config {
   static final Config _singleton = Config._internal();
 
   // Social Login Secret Keys
-  // 1. 카카오
+  // 1. 구글
+  String get googleClientId => env('GOOGLE_CLIENT_ID');
+  String get googleClientSecret => env('GOOGLE_CLIENT_SECRET');
+  // 2. 카카오
   String get nativeAppKey => env('NATIVE_APP_KEY');
   String get javaScriptAppKey => env('JAVASCRIPT_APP_KEY');
   String get kakaoClientId => env('KAKAO_CLIENT_ID');
-  // 2. 네이버
+  // 3. 네이버
   String get naverClientId => env('NAVER_CLIENT_ID');
   String get naverClientSecret => env('NAVER_CLIENT_SECRET');
-  // 3. 애플
+  // 4. 애플
   String get appleClientId => env('APPLE_CLIENT_ID');
-  // 4. FCM
+  // 5. FCM
   String get webFcmKey => env('WEB_FCM_KEY');
   // 웹 로그인 시, Redirect 할 URL
   String get socialRedirectUrl => env('SOCIAL_REDIRECT_URL');
