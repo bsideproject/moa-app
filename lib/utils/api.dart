474d8e973d44f6128e1e47eab1175cf171657a5b
nain93 <rnrb555@gmail.com>
2023-06-22 20:49:19 +0900

    chore: api 주소 변경

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -3,7 +3,7 @@ import 'package:moa_app/utils/config.dart';
 
 Dio get dio {
   BaseOptions options = BaseOptions(
-    baseUrl: Config().moaDjangoUrl,
+    baseUrl: Config().baseUrl,
     // connectTimeout: const Duration(seconds: 5),
     // receiveTimeout: const Duration(seconds: 3),
   );
