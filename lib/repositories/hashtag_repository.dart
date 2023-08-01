6fa27d5b088c565a1cd685e2f33e8c98cc0d9a8c
namkyu koo(구남규) <rnrb555@gmail.com>
2023-08-01 20:54:49 +0900

    Feat/hash view loadmore (#49)
    * feat: 해시태그뷰 api 타입수정 및 페이지네이션

--- lib/repositories/hashtag_repository.dart
+++ lib/repositories/hashtag_repository.dart
@@ -5,7 +5,10 @@ import 'package:moa_app/repositories/token_repository.dart';
 import 'package:moa_app/utils/api.dart';
 
 abstract class IHashtagRepository {
-  Future<(List<ContentModel>, int)> getHashtagView();
+  Future<(List<ContentModel>, int)> getHashtagView({
+    int? page,
+    int? size,
+  });
   Future<List<HashtagModel>> getHashtagList();
 }
 
@@ -14,11 +17,14 @@ class HashtagRepository implements IHashtagRepository {
   static HashtagRepository instance = const HashtagRepository._();
 
   @override
-  Future<(List<ContentModel>, int)> getHashtagView() async {
+  Future<(List<ContentModel>, int)> getHashtagView({
+    int? page = 0,
+    int? size = 10,
+  }) async {
     var token = await TokenRepository.instance.getToken();
 
     var res = await dio.get(
-      '/api/v1/hashtag/view',
+      '/api/v1/hashtag/view?page=$page&size=$size',
       options: Options(
         headers: {
           'Authorization': 'Bearer $token',
@@ -26,9 +32,19 @@ class HashtagRepository implements IHashtagRepository {
       ),
     );
 
+    /// 백엔드 ContentModel 타입이 통일되지 않았으므로 임시로 타입 변환해서 넣어줌
     return (
       res.data['data']
-          .map<ContentModel>((e) => ContentModel.fromJson(e))
+          .map<ContentModel>(
+            (e) => ContentModel.fromJson({
+              'contentId': e['contentId'],
+              'contentImageUrl': e['imageUrl'],
+              'contentUrl': e['contentUrl'] ?? '',
+              'contentMemo': e['memo'],
+              'contentName': e['name'],
+              'contentHashTag': e['hashTags'],
+            }),
+          )
           .toList() as List<ContentModel>,
       res.data['count'] as int
     );
